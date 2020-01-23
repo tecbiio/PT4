127daf6c66e244b90dd4f612b2b91499cc7443fd
TecBioo <antony.lorenzelli1@etu.univ-lorraine.fr>
2020-01-23 11:05:35 +0100

    test sucess

--- TestMovementCube.py
+++ TestMovementCube.py
@@ -3,6 +3,11 @@ import MovementCube
 
 cube = RubiksCube.RubiksCube()
 mc = MovementCube.Movement()
+print("#########################################")
 print(cube.getTab())
 mc.right(cube)
+print("#########################################")
+print(cube.getTab())
+mc.left(cube)
+print("#########################################")
 print(cube.getTab())
